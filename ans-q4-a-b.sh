b1cec0ec0ae59c79a703c52da0a4ebe6a98dae6d
sanjeev.bagewadi <sanjeev.bagewadi@gmail.com>
2012-03-18 16:35:37 +0000

    Corrected ans-q4-a-b.sh and some changes to the dot file.

--- ans-q4-a-b.sh
+++ ans-q4-a-b.sh
@@ -12,6 +12,6 @@
 ./runge-kutta -h 0.01 -x 0 -l 0.8 -u 1 -p 3 > output-4-a.dat
 
 # Generate x(t) for lambda=0.6; mu=1.0; x(t=0) = 0; P1
-./runge-kutta -h 0.01 -x 0 -l 0.8  -u 1 -p 1 > output-4-b.dat
+./runge-kutta -h 0.01 -x 0 -l 0.6  -u 1 -p 1 > output-4-b.dat
 
 /usr/bin/gnuplot ans-q4-a-b-plot.p
